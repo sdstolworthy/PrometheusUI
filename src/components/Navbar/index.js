6cba33e6a8c0842d6769e34951d8c93a5ab6dc3d
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-06-04 18:58:35 -0600

    refactored navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -4,22 +4,32 @@ import { Link } from 'react-router-dom'
 import './navbar.css'
 import { propsToHelperClasses } from '../../helpers/componentHelpers';
 
-const Navbar = ({ children, logo, ...props }) => (
+const Navbar = ({ children, ...props }) => (
   <nav className={`navbar ${propsToHelperClasses(props)}`}>
     <div className="container">
-    <div className="navbar-brand">
-      <Link to="/" className="navbar-item">
-        <img src={logo} />
-      </Link>
-    </div>
-    <div className="navbar-menu">
-      <div className="navbar-end">
-        {children}
-      </div>
-    </div>
+      {children}
     </div>
   </nav>
 )
+
+Navbar.Menu = ({ children, ...props }) => (
+  <div className={`navbar-menu ${propsToHelperClasses(props)}`}>
+    {children}
+  </div>
+)
+
+Navbar.Burger = ({ children, ...props }) => (
+  <div className={`navbar-burger burger ${propsToHelperClasses(props)}`}>
+    {children}
+  </div>
+)
+
+Navbar.Brand = ({ children, ...props }) => (
+  <div className={`navbar-brand ${propsToHelperClasses(props)}`}>
+    {children}
+  </div>
+)
+
 Navbar.Link = ({ children, to, ...props }) => (
   <Link to={to} className={`navbar-item ${propsToHelperClasses(props)}`} >
     {children}
