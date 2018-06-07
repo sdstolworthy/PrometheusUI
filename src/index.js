e54f18374a811d6dee5d77d29ecf2b55487a9d2b
Spencer Stolworthy <sdstolworthy@gmail.com>
2018-06-07 09:12:56 -0600

    updated exports

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import 'bulma/css/bulma.css'
-export { default as Navbar } from './components/Navbar'
-export { default as Hero } from './components/Hero'
+
+/** Components */
 export { default as Footer } from './components/Footer'
+export { default as Hero } from './components/Hero'
 export { default as Modal } from './components/Modal'
+export { default as Navbar } from './components/Navbar'
+
+/** Layout */
 export { default as Container } from './layout/Container'
-export { default as Section } from './layout/Section'
-export { default as Button } from './elements/Button'
 export { default as Level } from './layout/Level'
+export { default as Section } from './layout/Section'
+
+/** Elements */
+export { default as Button } from './elements/Button'
 export { default as Content } from './elements/Content'
+export { default as Title } from './elements/Title'
